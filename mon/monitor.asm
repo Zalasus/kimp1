383a3def71e8387a2a72893aa7ed2ac54ecbddaa
Zalasus <niklas.weissner@gmail.com>
2014-12-15 15:58:40 +0100

    Added memory check

--- mon/monitor.asm
+++ mon/monitor.asm
@@ -33,6 +33,82 @@ org $0000
 ld HL, FFFFh ; init stackpointer to end of memory
 ld SP,HL
 
+; perform memory check    
+memCheck:
+    ld HL, ROM_END
+    ld B, $FF
+    
+memCheck_loop:
+    ld (HL), B
+    ld A, (HL)
+    cp B
+    jp NZ memCheck_error
+    inc HL
+    
+    ; we are done when HL goes zero
+    ld A, $00 
+    cp H
+    jp NZ, memCheck_loop
+    cp L
+    jp NZ, memCheck_loop
+    
+    ; high memory check successful. now we load the LOMEM checker into HIMEM
+    ld HL, loMemCheck
+    ld DE, ROM_END
+    ld BC, loMemCheck_end - loMemCheck
+    ldir
+    ; lomem checker routine is stored in HIMEM now.
+    ; jump to the loaded routine.
+    jp phase_loMemCheck
+    ; the routine jumps to the right handler after the check
+    
+memCheck_error:
+    ; there was a fatal memory error. try to print error message and halt
+    ld HL, str_memError
+    call printString
+    hlt
+    jp memCheck
+    
+
+; this routine is loaded into HIMEM by the memory check routine in order to
+; check LOMEM
+loMemCheck: 
+phase ROM_END ; thanks to phase, this routine now lies in RAM(HIMEM)
+phase_loMemCheck:
+    ld A, (1 << BIT_ROM_GATE) ; turn off the ROM mapping
+    out (IO_TCCR),A ; LOMEM is now accessible
+
+    ld HL, 0
+    
+phase_loMemCheck_loop:
+    ld (HL), B
+    ld A, (HL)
+    cp B
+    jp NZ phase_loMemCheck_error
+    inc HL
+    ; we are done when HL goes ROM_END
+    ld A, high ROM_END
+    cp H
+    jp NZ, phase_loMemCheck_loop
+    ld A, low ROM_END
+    cp L
+    jp NZ, phase_loMemCheck_loop
+    
+    ; no errors. turn ROM mapping back on. check ends here
+    ld A, 0
+    out (IO_TCCR), A ; ROM mapping is now back on
+    jp memCheck_ok
+    
+phase_loMemCheck_error:
+    ld A, 0
+    out (IO_TCCR), A ; ROM mapping is now back on
+    jp memCheck_error
+    
+dephase
+loMemCheck:
+
+memCheck_ok:
+
 jp monitorStart ; jump to monitor setup
 
 
@@ -47,6 +123,9 @@ str_unknownCommand:
     
 str_loading:
     db 'LOADING. PLEASE WAIT...', $0A, $0D, $00
+    
+str_memError:
+    db 'MEMORY ERROR', $0A, $0D, $00
     
 ; prints the character stored in A
 printChar:
@@ -82,6 +161,11 @@ printNewLine:
     ld A, $0D
     call printString
     
+    ret
+
+; clears the screen (requires VT100 terminal)
+clearScreen:
+
     ret
     
     
@@ -123,7 +207,6 @@ readString_end:
     pop HL ; restore HL
     ret ; character was TERM_LF, so return
     
-
     
 monitorStart:
     ; inititalize TCCR and perform an IO-RESET before intitalizing peripherals
@@ -159,6 +242,9 @@ monitorStart:
     
     ; IO-Devices are now initialized
     
+  
+    call clearScreen
+  
 monitor_welcome:
     ld HL, str_welcome ; print welcome message
     call printString
@@ -193,6 +279,7 @@ monitorPromt_loop:
     call printString
     
     jp monitorPromt_loop
+    
     
 ; parses a single hex character at (HL), stores result in A. 
 ; A = $FF means error
